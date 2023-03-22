dc204f4a6b91b4067104cd38d245c945ac4dfbfe
henriquewallace <henrique.wallace@hotmail.com>
2023-03-22 00:53:44 -0300

    Create persistence layer with dynamoDB configs and ScheduleRepository

---
+++ src/main/java/com/una/persistence/ScheduleRepository.java
@@ -0,0 +1,13 @@
+package com.una.persistency;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.una.entidades.Schedule;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan()
+public interface ScheduleRepository extends CrudRepository<Schedule, UUID> {
+    List<Schedule> findById(String id);
+}
