7a297de57b12c8567a3f82b4cc6ef9fc7914b75e
henriquewallace <henrique.wallace@hotmail.com>
2023-03-22 00:55:40 -0300

    Fix pom.xml dependencies

--- src/main/java/com/una/persistence/DynamoDbConfig.java
+++ src/main/java/com/una/persistence/DynamoDbConfig.java
@@ -1,4 +1,4 @@
-package com.una.persistency;
+package com.una.persistence;
 
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.Protocol;
@@ -7,7 +7,6 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- src/main/java/com/una/persistence/ScheduleRepository.java
+++ src/main/java/com/una/persistence/ScheduleRepository.java
@@ -1,4 +1,4 @@
-package com.una.persistency;
+package com.una.persistence;
 
 import java.util.List;
 import java.util.UUID;
