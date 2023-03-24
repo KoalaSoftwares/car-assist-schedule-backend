3bc52b2a507b5a83d16429742f7e098e957a4029
henriquewallace <henrique.wallace@hotmail.com>
2023-03-23 21:43:59 -0300

    Create override methods to service class

--- src/main/java/com/una/entidades/Service.java
+++ src/main/java/com/una/entidades/Service.java
@@ -47,4 +47,19 @@ public class Service {
     public void setPrice(int price) {
         this.price = price;
     }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
