a39c4aafc691d1538d47f4c1c63711cb7e727888
henriquewallace <henrique.wallace@hotmail.com>
2023-03-24 00:00:24 -0300

    Add junit dependencies

--- src/main/java/com/una/carassistschedulebackend/persistence/ScheduleRepository.java
+++ src/main/java/com/una/carassistschedulebackend/persistence/ScheduleRepository.java
@@ -6,8 +6,11 @@ import java.util.UUID;
 import com.una.entidades.Schedule;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 @EnableScan()
 public interface ScheduleRepository extends CrudRepository<Schedule, UUID> {
     List<Schedule> findById(String id);
+    List<Schedule> findByClientName(String clientName);
 }
